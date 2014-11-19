fa874d123757f93bc34c087b2c85572230b9ec00
Arthur Longoni <arthur_holiver@hotmail.com>
2014-11-19 15:43:07 -0200

    adicionei botão new nos members e criei uma página inicial temporaria

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TimeSheet::Application.routes.draw do
+  get "welcome/index"
   resources :members
   resources :roles
  
@@ -9,7 +10,7 @@ TimeSheet::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
